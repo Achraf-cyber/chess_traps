246773a688a4f8e31e6a4f334d007c90fb914762
Achraf-cyber <84545678+Achraf-cyber@users.noreply.github.com>
2026-03-14 00:39:26 +0000

    chore(splash): remove splash_master package as it is not used

--- lib/features/home/view/main_subscreen.dart
+++ lib/features/home/view/main_subscreen.dart
@@ -1,4 +1,4 @@
-import 'package:chess_traps/data/fens.dart' as fens;
+import 'package:chess_traps/data/fens.dart';
 import 'package:chess_traps/generated/assets.dart';
 import 'package:chess_traps/utils.dart';
 import 'package:chess_traps/widgets/trap_with_name.dart';
@@ -14,7 +14,20 @@ class MainSubscreen extends StatelessWidget {
       children: [
         Image.asset(AppAssets.imagesHomeImagePng),
         Row(children: [TrapWithName(), TrapWithName()]),
-        Column(children: [Text(context.phrase.recentTraps)]),
+        Column(
+          children: [
+            Text(context.phrase.recentTraps),
+            Row(
+              children: [
+                Chessboard.fixed(
+                  size: 200,
+                  orientation: .white,
+                  fen: initialBoardFen,
+                ),
+              ],
+            ),
+          ],
+        ),
       ],
     );
   }
